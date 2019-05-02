187cb8f1a5367df1f2567119c4ca6a56d96058e1
Scott Black <scott.black@usu.edu>
2019-05-02 15:27:27 -0600

    add extra account registration fields

---
+++ custom_reg_form/models.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from django.db import models
+
+# Backwards compatible settings.AUTH_USER_MODEL
+USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+
+class ExtraInfo(models.Model):
+    """
+    This model contains two extra fields that will be saved when a user registers.
+    The form that wraps this model is in the forms.py file.
+    """
+    user = models.OneToOneField(USER_MODEL, null=True)
+    USAGE = (
+        ('', ''),
+        ('instructor', 'Instructor'),
+        ('student', 'Student'),
+    )
+
+    goals = models.CharField(
+        verbose_name="Goals",
+        max_length=100,
+        default="",
+    )
+    organization = models.CharField(
+        verbose_name="University or Organization",
+        max_length=100,
+        default="",
+    )
+    usage = models.CharField(
+        verbose_name="Usage",
+        choices=USAGE,
+        default="",
+        max_length=10,
+    )
+
