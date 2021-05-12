74a2a6e5ffc60f483a6957ad7058de9ccf091b5e
Scott Black <scott.black@usu.edu>
2021-05-12 10:42:55 -0600

    squash rebase course export functionality onto koa

--- lms/djangoapps/courseware/views/views.py
+++ lms/djangoapps/courseware/views/views.py
@@ -937,6 +937,7 @@ def course_about(request, course_id):
 
         staff_access = bool(has_access(request.user, 'staff', course))
         studio_url = get_studio_url(course, 'settings/details')
+        studio_export_url = get_studio_url(course, 'export')
 
         if request.user.has_perm(VIEW_COURSE_HOME, course):
             if course_home_legacy_is_active(course.id):
@@ -1002,6 +1003,7 @@ def course_about(request, course_id):
             'course_details': course_details,
             'staff_access': staff_access,
             'studio_url': studio_url,
+            'studio_export_url': studio_export_url,
             'registered': registered,
             'course_target': course_target,
             'is_cosmetic_price_enabled': settings.FEATURES.get('ENABLE_COSMETIC_DISPLAY_PRICE'),
